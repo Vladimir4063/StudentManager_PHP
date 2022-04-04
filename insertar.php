043f0a841a7cb8665a8e4ae4764a14f279e3e6ce
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2022-04-04 19:00:13 -0300

    second commit

--- insertar.php
+++ insertar.php
@@ -2,17 +2,17 @@
 include("conexion.php");
 $con = conectar();
 
-$dni = $_POST['dni'];
 $nombres = $_POST['nombres'];
 $apellidos = $_POST['apellidos'];
+$dni = $_POST['dni'];
 
-$sql = "INSERT INTO alumnos VALUES ('$dni','$nombres','$apellidos')";
+$sql = "INSERT INTO alumnos VALUES ($dni,'$nombres','$apellidos')";
 $query = mysqli_query($con, $sql);
+
 
 if($query){
     Header("Location: alumnos.php");
 }else{
-    
 }
 
 ?>
